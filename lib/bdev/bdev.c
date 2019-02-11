johnmeneghini/spdk

commit 2c2a5fbb796b62b7a437f34bbcef73e77e9d3629

nvmf_fc: Add a new state to enum spdk_bdev_io_status
Added a new state called SPDK_BDEV_IO_STATUS_INIT. This represents a request whose bdev_io has been initialized but not yet submitted to the backend block device.
Only formatting/indentation changes made to nvmf_fc_ls.c.

lib/bdev/bdev.c
@@ -616,7 +616,7 @@ spdk_bdev_io_resubmit(struct spdk_bdev_io *bdev_io, struct spdk_bdev_desc *new_b
 {
 	struct spdk_bdev *new_bdev = new_bdev_desc->bdev;
 
-	assert(bdev_io->status == SPDK_BDEV_IO_STATUS_PENDING);
+	assert(bdev_io->status == SPDK_BDEV_IO_STATUS_INIT);
 	bdev_io->bdev = new_bdev;
 
 	/*
@@ -643,7 +643,7 @@ spdk_bdev_io_init(struct spdk_bdev_io *bdev_io,
 	bdev_io->caller_ctx = cb_arg;
 	bdev_io->cb = cb;
 	bdev_io->gencnt = bdev->gencnt;
-	bdev_io->status = SPDK_BDEV_IO_STATUS_PENDING;
+	bdev_io->status = SPDK_BDEV_IO_STATUS_INIT;
 	bdev_io->in_submit_request = false;
 }
 

include/spdk_internal/bdev.h
@@ -187,6 +187,8 @@ enum spdk_bdev_io_status {
 	SPDK_BDEV_IO_STATUS_FAILED = -1,
 	SPDK_BDEV_IO_STATUS_PENDING = 0,
 	SPDK_BDEV_IO_STATUS_SUCCESS = 1,
+	SPDK_BDEV_IO_STATUS_INIT = 2,
+
 };
 
 struct spdk_bdev {

lib/bdev/pbdev.c
@@ -366,7 +366,7 @@ __submit_request(struct spdk_bdev *bdev, struct spdk_bdev_io *bdev_io)
 {
 	struct spdk_io_channel *ch = NULL;
 
-	assert(bdev_io->status == SPDK_BDEV_IO_STATUS_PENDING);
+	assert(bdev_io->status == SPDK_BDEV_IO_STATUS_INIT);
 
 	bdev_io->in_submit_request = true;
 	bdev->fn_table->submit_request(ch, bdev_io);
@@ -391,7 +391,7 @@ spdk_bdev_io_init(struct spdk_bdev_io *bdev_io,
 	bdev_io->caller_ctx = cb_arg;
 	bdev_io->cb = cb;
 	bdev_io->gencnt = bdev->gencnt;
-	bdev_io->status = SPDK_BDEV_IO_STATUS_PENDING;
+	bdev_io->status = SPDK_BDEV_IO_STATUS_INIT;
 	bdev_io->in_submit_request = false;
 }
 

commit d12ee784142c69b66176f707c8071367a56765ac

nvmf_fc: Fix invalid assert in bdev.c.
This is a regression introduced by 2c2a5fbb7 commit.

lib/bdev/bdev.c
@@ -592,7 +592,7 @@ __submit_request(struct spdk_bdev *bdev, struct spdk_bdev_io *bdev_io)
 {
 	struct spdk_io_channel *ch;
 
-	assert(bdev_io->status == SPDK_BDEV_IO_STATUS_PENDING);
+	assert(bdev_io->status == SPDK_BDEV_IO_STATUS_INIT);
 
 	ch = bdev_io->ch->channel;
 