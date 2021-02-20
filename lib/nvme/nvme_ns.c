c4342677b154f48222d81ddfb232ea74c6914e18
Ed rodriguez <edwinr@netapp.com>
2021-02-20 06:32:23 -0500

    nvme: Support for IOCS specific data

--- lib/nvme/nvme_ns.c
+++ lib/nvme/nvme_ns.c
@@ -321,6 +321,7 @@ spdk_nvme_ns_get_id(struct spdk_nvme_ns *ns)
 bool
 spdk_nvme_ns_is_active(struct spdk_nvme_ns *ns)
 {
+	const struct spdk_nvme_ns_data *nsdata = NULL;
 
 	/*
 	 * According to the spec, valid NS has non-zero id.
@@ -329,27 +330,14 @@ spdk_nvme_ns_is_active(struct spdk_nvme_ns *ns)
 		return false;
 	}
 
-	if (ns->csi == SPDK_NVME_CSI_KV) {
-		const struct spdk_nvme_kv_ns_data *nsdata_kv = NULL;
-		nsdata_kv = spdk_nvme_kv_ns_get_data(ns);
+	nsdata = _nvme_ns_get_data(ns);
 
-		/*
-		 * According to the spec, Identify Namespace will return a zero-filled structure for
-		 *  inactive namespace IDs.
-		 * Check NSZE since it must be nonzero for an active namespace.
-		 */
-		return nsdata_kv->nsze != 0;
-	} else {
-		const struct spdk_nvme_ns_data *nsdata = NULL;
-		nsdata = _nvme_ns_get_data(ns);
-
-		/*
-		 * According to the spec, Identify Namespace will return a zero-filled structure for
-		 *  inactive namespace IDs.
-		 * Check NCAP since it must be nonzero for an active namespace.
-		 */
-		return nsdata->ncap != 0;
-	}
+	/*
+	 * According to the spec, Identify Namespace will return a zero-filled structure for
+	 *  inactive namespace IDs.
+	 * Check NCAP since it must be nonzero for an active namespace.
+	 */
+	return nsdata->ncap != 0;
 }
 
 struct spdk_nvme_ctrlr *
@@ -571,6 +559,8 @@ nvme_ns_has_supported_iocs_specific_data(struct spdk_nvme_ns *ns)
 		 */
 		return false;
 	case SPDK_NVME_CSI_ZNS:
+		return true;
+	case SPDK_NVME_CSI_KV:
 		return true;
 	default:
 		SPDK_WARNLOG("Unsupported CSI: %u for NSID: %u\n", ns->csi, ns->id);
